e8beaddddbabfc81d3ba0f179b940b52a634f567
CoolDude145 <alexmovie@net.hr>
2020-04-25 19:18:15 +0200

    new sounds json file

--- src/main/java/me/coodlude/edgeofdarkness/common/init/ModItems.java
+++ src/main/java/me/coodlude/edgeofdarkness/common/init/ModItems.java
@@ -4,6 +4,7 @@ import me.coodlude.edgeofdarkness.EdgeOfDarkness;
 import me.coodlude.edgeofdarkness.common.items.ItemTardisKey;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
@@ -20,9 +21,11 @@ public class ModItems {
 
 
     public static Item tardis_key;
+    public static Item jammy_dodgers;
 
     public static void init() {
         tardis_key = registerItem(new ItemTardisKey(), "tardis_key").setCreativeTab(ModTabs.edgeofdarkness);
+        jammy_dodgers = registerItem(new ItemFood(1,0.5f,false),  "jammy_dodgers").setCreativeTab(ModTabs.edgeofdarkness);
     }
 
     public static Item registerItem(Item item, String name) {
