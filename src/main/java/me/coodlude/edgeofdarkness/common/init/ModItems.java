6c5429dd2186d0a6e8887e4958e977dea70a4378
CoolDude145 <alexmovie@net.hr>
2020-04-26 10:57:37 +0200

    emerald stuff

--- src/main/java/me/coodlude/edgeofdarkness/common/init/ModItems.java
+++ src/main/java/me/coodlude/edgeofdarkness/common/init/ModItems.java
@@ -4,9 +4,7 @@ import me.coodlude.edgeofdarkness.EdgeOfDarkness;
 import me.coodlude.edgeofdarkness.common.items.ItemSonicScrewdriver;
 import me.coodlude.edgeofdarkness.common.items.ItemTardisKey;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemFood;
-import net.minecraft.item.ItemSword;
+import net.minecraft.item.*;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
@@ -26,12 +24,17 @@ public class ModItems {
     public static Item jammy_dodgers;
     public static Item sonic_screwdriver;
     public static Item emerald_sword;
+    public static Item emerald_pickaxe;
+    public static Item emerald_axe;
 
     public static void init() {
         tardis_key = registerItem(new ItemTardisKey(), "tardis_key").setCreativeTab(ModTabs.edgeofdarkness);
         jammy_dodgers = registerItem(new ItemFood(1, 0.5f, false), "jammy_dodgers").setCreativeTab(ModTabs.edgeofdarkness);
         sonic_screwdriver = registerItem(new ItemSonicScrewdriver(), "sonic_screwdriver").setCreativeTab(ModTabs.edgeofdarkness);
         emerald_sword = registerItem(new ItemSword(Item.ToolMaterial.DIAMOND),  "emerald_sword").setCreativeTab(ModTabs.edgeofdarkness);
+        emerald_pickaxe = registerItem(new ItemPickaxe(Item.ToolMaterial.DIAMOND),  "emerald_pickaxe").setCreativeTab(ModTabs.edgeofdarkness);
+        emerald_axe = registerItem(new ItemAxe(Item.ToolMaterial.DIAMOND), "emerald_axe").setCreativeTab(ModTabs.edgeofdarkness);
+
     }
 
     public static Item registerItem(Item item, String name) {
