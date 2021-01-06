deeedde32d59bcb018493729d022a1016818040f
TheGingerDoctor <cormac.moroney@gmail.com>
2021-01-06 12:53:32 +0000

    Interesting...

--- src/main/java/me/coodlude/edgeofdarkness/client/models/ModelDoor.java
+++ src/main/java/me/coodlude/edgeofdarkness/client/models/ModelDoor.java
@@ -22,7 +22,7 @@ public class ModelDoor extends ModelBase {
 		textureHeight = 256;
 
 		MainBody = new ModelRenderer(this);
-		MainBody.setRotationPoint(0.0F, 32.0F, 0.0F);
+		MainBody.setRotationPoint(0.0F, 34.0F, 0.0F);
 		MainBody.cubeList.add(new ModelBox(MainBody, 174, 38, -13.0F, -55.0F, -2.5F, 3, 55, 5, 0.0F, false));
 		MainBody.cubeList.add(new ModelBox(MainBody, 190, 38, 10.0F, -55.0F, -2.5F, 3, 55, 5, 0.0F, false));
 		MainBody.cubeList.add(new ModelBox(MainBody, 174, 98, -12.0F, -57.0F, -1.5F, 2, 2, 3, 0.0F, false));
@@ -31,12 +31,12 @@ public class ModelDoor extends ModelBase {
 		MainBody.cubeList.add(new ModelBox(MainBody, 82, 110, -14.0F, -53.0F, -2.5F, 28, 4, 5, 0.0F, false));
 
 		LeftDoor = new ModelRenderer(this);
-		LeftDoor.setRotationPoint(10.0F, 32.0F, 0.5F);
+		LeftDoor.setRotationPoint(10.0F, 34.0F, 0.5F);
 		LeftDoor.cubeList.add(new ModelBox(LeftDoor, 44, 161, -10.0F, -49.0F, -1.0F, 10, 49, 1, 0.0F, false));
 		LeftDoor.cubeList.add(new ModelBox(LeftDoor, 12, 0, -2.0F, -32.0F, 0.0F, 1, 3, 1, 0.0F, false));
 
 		RightDoor = new ModelRenderer(this);
-		RightDoor.setRotationPoint(-10.0F, 32.0F, 0.5F);
+		RightDoor.setRotationPoint(-10.0F, 34.0F, 0.5F);
 		RightDoor.cubeList.add(new ModelBox(RightDoor, 66, 161, 0.0F, -49.0F, -1.0F, 10, 49, 1, 0.0F, false));
 		RightDoor.cubeList.add(new ModelBox(RightDoor, 4, 22, 8.0F, -27.0F, 0.0F, 1, 5, 1, 0.0F, false));
 	}
